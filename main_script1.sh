a6d928e25cb32626e0be547985d95febf2bd9235
tkurbanova <74053256+tkurbanova@users.noreply.github.com>
2020-11-06 13:38:39 +0100

    Update main_script1.sh

--- main_script1.sh
+++ main_script1.sh
@@ -12,7 +12,7 @@ mkdir scripts
 
 qsub scripts/qc_script.sh
 #or
-#qsub ­-l walltime=4:0:0 ­q- default ­l- select=1:ncpus=6:mem=4gb:scratch_local=100gb qc_script.sh
+#qsub ­-l walltime=4:0:0 ­-q default ­-l select=1:ncpus=6:mem=4gb:scratch_local=100gb qc_script.sh
 
 
 
